97b947096e476e2a8dbe0e8a729a178797c864ec
Tiago Porto <tiagoporto@ymail.com>
2019-04-24 22:58:26 -0300

    Add create-react-app

---
+++ src/App.js
@@ -0,0 +1,356 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <header role="banner">
+        <h1>HTML5 Test Page</h1>
+        <p>
+          This is a test page filled with common HTML elements to be used to
+          provide visual feedback whilst building CSS systems and frameworks.
+        </p>
+      </header>
+
+      <main>
+        <h1>Adjust font size to 36</h1>
+        <h2>
+          Adjust font size to 36 Adjust font size to 36 Adjust font size to 36
+          Adjust font size to 36 Adjust font size to 36 Adjust font size to 36
+          Adjust font size to 36
+        </h2>
+        <h3>
+          Adjust font size to 36 Adjust font size to 36 Adjust font size to 36
+          Adjust font size to 36 Adjust font size to 36 Adjust font size to 36
+          Adjust font size to 36
+        </h3>
+        <h4>Adjust font size to 36</h4>
+        <h5>h5.Swill Boilerplate</h5>
+        <h6>h6.Swill Boilerplate</h6>
+
+        <div />
+        <p>
+          First paragraph, <strong>consectetur</strong> adipisicing elit. Ipsam{" "}
+          <kbd>kbd tag</kbd> dolore deserunt laboriosam, perferendis quia earum{" "}
+          <kbd>kbd tag</kbd> quam accusamus consequuntur explicabo modi debitis{" "}
+          <mark>placeat</mark> eveniet blanditiis esse incidunt commodi
+          assumenda. Enim repellendus vitae eaque distinctio velit quia modi,
+          non maxime reiciendis amet ab temporibus! Aliquid illo natus,
+          architecto minima saepe cupiditate autem dolor, odit! Dolorem beatae
+          est iste incidunt quaerat delectus nobis error dignissimos numquam,
+          voluptatem necessitatibus <abbr title="Swill Boilerplate">SWBP</abbr>{" "}
+          exercitationem at sunt quidem voluptatibus nam facilis veniam nulla
+          odit dolorum pariatur, laboriosam?{" "}
+          <em>
+            Quam nisi non delectus perspiciatis veritatis harum iure maxime,
+            quae voluptatibus.
+          </em>
+        </p>
+        <p>
+          Second paragraph,{" "}
+          <a href="https://www.npmjs.com/package/generator-swill-boilerplate">
+            this is a link to NPM boilerplate package
+          </a>
+          . Deserunt est rem architecto rerum ea repellat eveniet assumenda
+          officiis atque! Et.
+        </p>
+        <p>
+          <u>Lorem ipsum dolor sit amet.</u>
+        </p>
+        <p>
+          <del>Lorem ipsum dolor sit amet.</del>
+        </p>
+        <p>
+          <small>
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Perspiciatis, modi, exercitationem. Tempora
+            adipisci at eligendi in, laborum quia atque natus asperiores cum
+            reprehenderit. Nihil magnam sint blanditiis culpa veniam, dolores
+            doloribus atque velit neque repellat ullam quod odio id officia
+            officiis repudiandae eius excepturi consectetur natus reiciendis at!
+            Expedita, molestias.
+          </small>
+        </p>
+        <blockquote>
+          <p>
+            The <kbd>&lt;blockquote&gt;</kbd> tag specifies a section that is
+            quoted from another source. Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Atque inventore aspernatur eligendi facere culpa
+            in dolorem, assumenda deserunt omnis provident.
+          </p>
+          <footer>Lorem ipsum dolor sit amet.</footer>
+        </blockquote>
+        <code>
+          &lt;div&gt; &lt;input type="text" placeholder="It's just a code
+          example"&gt; &lt;/div&gt;
+        </code>
+        <pre />
+        <p>
+          Lorem ipsum dolor sit amet, <code>Inline code</code>. Maxime,
+          quibusdam.
+        </p>
+        <pre>
+          <code>
+            &lt;div&gt; &lt;p&gt;Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Laudantium eaque blanditiis obcaecati alias rem,
+            voluptatum maiores porro deleniti tenetur aliquam cupiditate vel
+            ratione aliquid temporibus quas, sint expedita non, cumque ipsum
+            autem dolorum labore aperiam quidem. Odio molestias debitis, qui
+            eveniet possimus nemo tempore delectus. Voluptates, deserunt ipsum
+            neque et.&lt;/p&gt; &lt;p&gt;Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Laudantium eaque blanditiis obcaecati alias rem,
+            voluptatum maiores porro deleniti tenetur aliquam cupiditate vel
+            ratione aliquid temporibus quas, sint expedita non, cumque ipsum
+            autem dolorum labore aperiam quidem. Odio molestias debitis, qui
+            eveniet possimus nemo tempore delectus. Voluptates, deserunt ipsum
+            neque et.&lt;/p&gt; &lt;p&gt;Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Laudantium eaque blanditiis obcaecati alias rem,
+            voluptatum maiores porro deleniti tenetur aliquam cupiditate vel
+            ratione aliquid temporibus quas, sint expedita non, cumque ipsum
+            autem dolorum labore aperiam quidem. Odio molestias debitis, qui
+            eveniet possimus nemo tempore delectus. Voluptates, deserunt ipsum
+            neque et.&lt;/p&gt; &lt;p&gt;Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Laudantium eaque blanditiis obcaecati alias rem,
+            voluptatum maiores porro deleniti tenetur aliquam cupiditate vel
+            ratione aliquid temporibus quas, sint expedita non, cumque ipsum
+            autem dolorum labore aperiam quidem. Odio molestias debitis, qui
+            eveniet possimus nemo tempore delectus. Voluptates, deserunt ipsum
+            neque et.&lt;/p&gt; &lt;p&gt;Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Laudantium eaque blanditiis obcaecati alias rem,
+            voluptatum maiores porro deleniti tenetur aliquam cupiditate vel
+            ratione aliquid temporibus quas, sint expedita non, cumque ipsum
+            autem dolorum labore aperiam quidem. Odio molestias debitis, qui
+            eveniet possimus nemo tempore delectus. Voluptates, deserunt ipsum
+            neque et.&lt;/p&gt; &lt;/div&gt;
+          </code>
+        </pre>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque non
+          voluptate vero autem repellat rem, suscipit consequatur eius dolor
+          maxime!
+        </p>
+        <figure>
+          <img
+            src="img_pulpit.jpg"
+            alt="The Pulpit Rock"
+            width="304"
+            height="228"
+          />
+          <figcaption>Fig1. - A view of the pulpit rock in Norway.</figcaption>
+        </figure>
+        <ul>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>
+            Lorem ipsum dolor sit amet.
+            <ul>
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>
+                Lorem ipsum dolor sit amet.
+                <ul>
+                  <li>Lorem ipsum dolor sit amet.</li>
+                  <li>Lorem ipsum dolor sit amet.</li>
+                  <li>Lorem ipsum dolor sit amet.</li>
+                  <li>Lorem ipsum dolor sit amet.</li>
+                </ul>
+              </li>
+              <li>Lorem ipsum dolor sit amet.</li>
+            </ul>
+          </li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>
+            Lorem ipsum dolor sit amet.
+            <ol>
+              <li>Lorem ipsum dolor sit.</li>
+              <li>
+                Lorem ipsum dolor sit.
+                <ol>
+                  <li>lorem3</li>
+                  <li>lorem3</li>
+                </ol>
+              </li>
+              <li>Lorem ipsum dolor sit.</li>
+            </ol>
+          </li>
+          <li>Lorem ipsum dolor sit amet.</li>
+        </ul>
+        <ol>
+          <li>
+            Lorem ipsum dolor sit amet.
+            <ol>
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>
+                Lorem ipsum dolor sit amet.
+                <ul>
+                  <li>Lorem ipsum dolor.</li>
+                  <li>Lorem ipsum dolor.</li>
+                  <li>Lorem ipsum dolor.</li>
+                </ul>
+              </li>
+              <li>Lorem ipsum dolor sit amet.</li>
+            </ol>
+          </li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+        </ol>
+        <dl>
+          <dt>Coffee</dt>
+          <dd>Black hot drink</dd>
+          <dt>Milk</dt>
+          <dd>White cold drink</dd>
+        </dl>
+        <h2>title</h2>
+        <hr />
+        <address>
+          <strong>Swill boilerplate</strong>
+          <br />
+          Website:{" "}
+          <a href="http://www.swillboileplate.rocks">swillboileplate.rocks</a>.
+          <br />
+          Tiago Porto
+          <br />
+          Belo Horizonte, MG
+          <br />
+          BRAZIL
+        </address>
+
+        <div style={{ background: "red" }}>
+          <p>twertwert</p>
+          <p>twertwert</p>
+          <p>twertwert</p>
+        </div>
+
+        <form action="#" method="POST">
+          <fieldset>
+            <legend>My Semantic Form</legend>
+            <p>
+              <label for="username">Username*</label>
+              <input
+                id="username"
+                type="text"
+                name="username"
+                maxlength="10"
+                aria-required="true"
+                required
+              />
+            </p>
+
+            <p>
+              <label for="email">Email*</label>
+              <input id="email" type="email" name="email" required />
+            </p>
+
+            <p>
+              <label for="disabled">Disabled</label>
+              <input id="disabled" type="text" name="disabled" disabled />
+            </p>
+
+            <p>
+              <label for="number">Number</label>
+              <input id="number" type="number" name="number" min="1" max="5" />
+            </p>
+
+            <p>
+              <label for="birthday">Birthday</label>
+              <input
+                id="birthday"
+                type="text"
+                maxlength="10"
+                name="birthday"
+                pattern="[0-9]{2}\/[0-9]{2}\/[0-9]{4}$"
+              />
+            </p>
+
+            <p>
+              <label for="hour">Hour</label>
+              <input
+                id="hour"
+                type="text"
+                maxlength="8"
+                name="hour"
+                pattern="[0-9]{2}:[0-9]{2} [0-9]{2}$"
+              />
+            </p>
+
+            <p>
+              <label for="hour">Range</label>
+              <input type="range" name="points" min="0" max="10" />
+            </p>
+
+            <p>
+              <label for="phone">Phone</label>
+              <input
+                id="phone"
+                type="text"
+                name="phone"
+                maxlength="15"
+                pattern="\([0-9]{2}\) [0-9]{4,6}-[0-9]{3,4}$"
+              />
+            </p>
+
+            <p>
+              <label>Sex</label>
+              <label>
+                <input type="radio" name="sex" value="male" checked />
+                Male
+              </label>
+              <label>
+                <input type="radio" name="sex" value="female" />
+                Female
+              </label>
+            </p>
+
+            <p>
+              <label>Checkbox</label>
+              <label>
+                <input type="checkbox" name="vehicle" value="Bike" /> I have a
+                bike
+              </label>
+              <label>
+                <input type="checkbox" name="vehicle" value="Car" /> I have a
+                car
+              </label>
+            </p>
+
+            <p>
+              <label id="choose2">Drop</label>
+              <select name="choose">
+                <optgroup label="Front-end">
+                  <option value="html">HTML</option>
+                  <option value="css">CSS</option>
+                </optgroup>
+                <optgroup label="Back-end">
+                  <option value="ruby">Ruby</option>
+                  <option value="python">Python</option>
+                  <option value="php">PHP</option>
+                </optgroup>
+              </select>
+            </p>
+
+            <p>
+              <label for="comment">Comment</label>
+              <textarea id="comment" name="comment" rows="3" cols="50" />
+            </p>
+            <p>
+              <input type="file" />
+            </p>
+            <input type="hidden" name="userID" value="1" />
+          </fieldset>
+
+          <fieldset>
+            <p>
+              <label for="password">Password</label>
+              <input id="password" type="password" name="password" />
+            </p>
+          </fieldset>
+          <button type="button" disabled>
+            cancel
+          </button>
+          <button type="submit">Send</button>
+        </form>
+      </main>
+    </>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import './google_analytics.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-if (module.hot) {
-  module.hot.accept()
-}
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
