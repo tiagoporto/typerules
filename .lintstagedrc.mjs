5f09e753c55cd9a9899bad5a3b3bf14c3571de2a
Tiago Porto <tiago@tiagoporto.com>
2025-01-20 14:02:00 -0300

    chore: setup stylelint

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,7 +1,7 @@
 export default {
   '*': 'prettier --check --ignore-unknown --write',
   '*.{md,markdown,mdx}': ['remark --frail', 'eslint --max-warnings 0'],
-  // '*.{scss,css}': 'stylelint',
+  '*.{scss,css}': 'stylelint --fix',
   // '*.{ts,tsx}': () => 'tsc --project tsconfig.json',
   '*.{js,mjs,cjs,jsx,ts,tsx}': [
     'eslint --max-warnings 0',
