eeed22acaeaa3772994060e36a5bc92b2332e120
Tiago Porto <tiago@tiagoporto.com>
2025-02-12 13:54:11 -0300

    chore: update dependencies

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,10 +1,14 @@
 export default {
   '*': 'prettier --check --ignore-unknown --write',
-  '*.{md,markdown,mdx}': ['remark --frail', 'eslint --max-warnings 0'],
+  '*.{md,markdown,mdx}': [
+    'remark --frail',
+    'eslint --max-warnings 0 --no-warn-ignored',
+  ],
+  '*.{html,yml,json,jsonc,json5}': 'eslint --max-warnings 0 --no-warn-ignored',
   '*.{scss,css}': 'stylelint --fix',
   // '*.{ts,tsx}': () => 'tsc --project tsconfig.json',
-  '*.{js,mjs,cjs,jsx,ts,tsx}': [
-    'eslint --max-warnings 0',
+  '*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}': [
+    'eslint --max-warnings 0 --no-warn-ignored',
     //   'jest --bail --findRelatedTests --passWithNoTests',
   ],
 }
