3b565b58c32012f86debbd5c0c46c55433f1bf9b
Tiago Porto <tiago@tiagoporto.com>
2025-05-24 23:22:45 -0300

    ci: set semantic release

---
+++ .releaserc.mjs
@@ -0,0 +1,46 @@
+const config = {
+  extends: 'semantic-release-monorepo',
+  branches: [
+    'main',
+    {
+      name: 'alpha',
+      channel: 'alpha',
+      prerelease: true,
+    },
+    {
+      name: 'beta',
+      channel: 'beta',
+      prerelease: true,
+    },
+  ],
+  preset: 'conventionalcommits',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [
+          {
+            type: 'docs',
+            release: 'minor',
+          },
+          {
+            type: 'refactor',
+            release: 'minor',
+          },
+        ],
+      },
+    ],
+  ],
+}
+
+if (process.env.BRANCH === 'main') {
+  config.plugins.push('@semantic-release/release-notes-generator')
+}
+
+config.plugins.push('@anolilab/semantic-release-pnpm')
+
+if (process.env.BRANCH === 'main') {
+  config.plugins.push('@semantic-release/github')
+}
+
+export default config
